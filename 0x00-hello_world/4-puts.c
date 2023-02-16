d5c3ec6e30e97b9b330992f26ec2b2716e04e840
gidcodess <gideonkhumalo2@gmail.com>
2023-02-16 04:59:36 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 
 
 /**
- * main - print Programming is like building a multilingual puzzle, followes by a newline.
+ * main - "print Programming is like building a multilingual 
+ * puzzle, followes by a newline.
  * Return: Always 0.
  */
 int main(void)
