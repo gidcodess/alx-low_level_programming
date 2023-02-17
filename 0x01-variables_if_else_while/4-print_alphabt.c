3e68e7c161e2131ecdcebb17b4b5d9f1872bc8d4
gidcodess <gideonkhumalo2@gmail.com>
2023-02-17 06:27:56 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,6 +5,7 @@
 /**
  * main - Entry point 
  * Return: Always 0 (Success)
+ *
  */
 
 int main(void)
@@ -21,5 +22,5 @@ int main(void)
 	putchar(low);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
