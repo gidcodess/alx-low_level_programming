41831f927d8a82292165f96a27b5f813c2bcfcd9
gidcodess <gideonkhumalo2@gmail.com>
2023-03-01 11:50:11 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,15 @@
  */
 
 int _strcmp(char *s1, char *s2)
+
 {
-	while (*s1 && *s2 && *s1 == *s2)
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
-	return (*s1 - *s2);
+	return (s1[i] - s2[i]);
 }
