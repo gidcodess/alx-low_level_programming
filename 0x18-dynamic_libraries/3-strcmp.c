2f2e3e1f84fc0b0401345cee842195baf6e03f4f
gidcodess <gideonkhumalo2@gmail.com>
2024-03-25 05:43:01 -0700

    task 0

---
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string to be compared.
+ * @s2: The second string to be compared.
+ *
+ * Return: If str1 < str2, return -1.
+ *         If str1 > str2, return 1.
+ *         Otherwise, return 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+
+	return (s1[i] - s2[i]);
+}
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,1 @@
+ dynamic libraries (also known as shared libraries) are compiled code files that contain functions, variables, and symbols that can be linked to a program at runtime.
